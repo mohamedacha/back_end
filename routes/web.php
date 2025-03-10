85a4e8c24a08605efa1a647e5d8af88865da09a9
ouichouani <ouichouaniabdlhakim@gmal.com>
2025-03-10 16:23:29 +0000

    fix api problems

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 
-Route::apiResource('users', UserController::class);
-Route::apiResource('orders', OrderController::class);
+
+
+Route::prefix('api')->middleware('api')->group(function () {
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('orders', OrderController::class);
+});
